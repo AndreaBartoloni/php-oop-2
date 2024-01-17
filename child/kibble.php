838feeb8990faf0e4345adbf3d469465fea61de0
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2024-01-17 14:03:42 +0100

    commit

--- child/kibble.php
+++ child/kibble.php
@@ -10,4 +10,4 @@ class Kibble extends Products
 }
 
 
-?>
+?> 
--- child/peluche.php
+++ child/peluche.php
@@ -2,6 +2,7 @@
     require_once __DIR__ . '/../product.php';
     class Peluche extends Products
     {
+        use Color;
         public $material;
         public $weight;
     }
